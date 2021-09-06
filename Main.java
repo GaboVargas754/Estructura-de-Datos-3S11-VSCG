07ae5986a0247aad78947e646e466139cf4dc132
GaboVargas754 <89992899+GaboVargas754@users.noreply.github.com>
2021-09-06 18:44:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,17 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
+        //Pedimos los datos al usuario
         System.out.print("Ingresa el tamaño de la matriz y vector: ");
         int tam = in.nextInt();
         
         System.err.println("Ejercicio 1");
+        //instanciamos la clase Main con la subclase Matriz 
         Matriz mt = new Matriz(tam);
-        mt.matriz();
+        mt.matriz();//traemos el método matriz de la clase Matriz
         
         System.err.println("\n--------------------------\n"
                 + "Ejercicio 2");
+        //instanciamos la clase Main con la subclase Vector
         Vector vt = new Vector(tam);
-        vt.vector();
+        vt.vector();//traemos el método vector de la clase Vector
         
     }
 }
